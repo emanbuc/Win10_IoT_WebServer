819ed43f19aa1f0e8e2982b4bb4b567f63e1cade
emanbuc <emanuele.buchicchio@informaticamente.it>
2016-08-21 12:13:34 +0200

    working webserver

---
+++ BackgroundWebServer/BackgroundWebServer/StartupTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using Windows.System.Threading;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace BackgroundWebServer
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        private static BackgroundTaskDeferral _Deferral = null;
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // If you start any asynchronous methods here, prevent the task
+            // from closing prematurely by using BackgroundTaskDeferral as
+            // described in http://aka.ms/backgroundtaskdeferral
+            //
+
+            _Deferral = taskInstance.GetDeferral();
+
+            var webserver = new WebServer();
+
+            await ThreadPool.RunAsync(workItem =>
+            {
+                webserver.Start();
+            });
+        }
+    }
+}
